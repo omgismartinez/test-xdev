efeee66be221d410e62186c2e5ae46731a2e2c39
omgismartinez <martnzomg@gmail.com>
2023-10-04 15:54:30 -0600

    🚀 feat: user form

---
+++ components/forms/user-form.tsx
@@ -0,0 +1,255 @@
+'use client'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import type * as z from 'zod'
+
+import { Button } from '@/components/ui/button'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { userSchema } from '@/lib/validations/user'
+import { useTransition } from 'react'
+import { toast } from 'sonner'
+import { catchError } from '@/lib/utils'
+import { newUserAction } from '@/app/actions/user'
+
+type Inputs = z.infer<typeof userSchema>
+
+const defaultValues: Partial<Inputs> = {
+  email: 'John@gmail.com',
+  username: 'johnd',
+  password: 'm38rmF$',
+  name: {
+    firstname: 'John',
+    lastname: 'Doe'
+  },
+  address: {
+    city: 'kilcoole',
+    street: '7835 new road',
+    number: 3,
+    zipcode: '12926-3874',
+    geolocation: {
+      lat: '-37.3159',
+      long: '81.1496'
+    }
+  },
+  phone: '1-570-236-7033'
+}
+
+interface UserFormProps {
+  user?: Inputs
+}
+
+export function UserForm ({ user }: UserFormProps) {
+  const [isPending, startTransition] = useTransition()
+
+  const form = useForm<Inputs>({
+    resolver: zodResolver(userSchema),
+    defaultValues: user ?? defaultValues
+  })
+
+  const isUser = !!user
+
+  function onSubmit (data: Inputs) {
+    console.log(data)
+    startTransition(async () => {
+      try {
+        if (isUser) {
+          // await updateUserAction(data)
+          toast.message('Actualizar estara disponible pronto.')
+        } else {
+          await newUserAction({
+            ...data
+          })
+          toast.success('Product added successfully.')
+        }
+
+        form.reset()
+      } catch (err) {
+        catchError(err)
+      }
+    })
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-4 grid'>
+        <FormField
+          control={form.control}
+          name='username'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Nombre de usuario</FormLabel>
+              <FormControl>
+                <Input placeholder='alvaro123' {...field} required />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='email'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Correo electrónico</FormLabel>
+              <FormControl>
+                <Input type={'email'} placeholder='alvaro@example.com' {...field} required />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='password'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Cotraseña</FormLabel>
+              <FormControl>
+                <Input type={'password'} placeholder='' {...field} required />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='name.firstname'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Nombre</FormLabel>
+              <FormControl>
+                <Input placeholder='alvaro' {...field} required />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='name.lastname'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Apellidos</FormLabel>
+              <FormControl>
+                <Input placeholder='martinez' {...field} required />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='address.city'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Ciudad</FormLabel>
+              <FormControl>
+                <Input placeholder='calle 123' {...field} required />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='address.street'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Calle</FormLabel>
+              <FormControl>
+                <Input placeholder='calle 123' {...field} required />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='address.number'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Número</FormLabel>
+              <FormControl>
+                <Input placeholder='6516' {...field} required />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='address.zipcode'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Código postal</FormLabel>
+              <FormControl>
+                <Input placeholder='94320' {...field} required />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='address.geolocation.lat'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Latitud</FormLabel>
+              <FormControl>
+                <Input placeholder='-37.3159' {...field} required />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='address.geolocation.long'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Longitud</FormLabel>
+              <FormControl>
+                <Input placeholder='81.1496' {...field} required />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='phone'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Teléfono</FormLabel>
+              <FormControl>
+                <Input placeholder='1234567890' {...field} required />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        {user
+          ? (
+              <Button type='submit' disabled={true}>
+                Actualizar
+              </Button>
+            )
+          : (
+              <Button type='submit' disabled={isPending}>
+                Crear usuario
+              </Button>
+            )}
+      </form>
+    </Form>
+  )
+}
