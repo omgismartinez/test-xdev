33e870b04da431f93473efdbb7dda859e2fe186d
omgismartinez <martnzomg@gmail.com>
2023-10-04 11:59:20 -0600

    🚀 feat (validations): `user schema` & `get user schema`

---
+++ lib/validations/user.ts
@@ -0,0 +1,29 @@
+import { z } from 'zod'
+
+export const userSchema = z.object({
+  id: z.number(),
+  email: z.string().email(),
+  username: z.string(),
+  password: z.string().optional(),
+  name: z.object({
+    firstname: z.string(),
+    lastname: z.string()
+  }),
+  address: z.object({
+    city: z.string(),
+    street: z.string(),
+    number: z.number(),
+    zipcode: z.string(),
+    geoLocation: z.object({
+      lat: z.string(),
+      long: z.string()
+    })
+  }),
+  phone: z.string()
+})
+
+export type User = z.infer<typeof userSchema>
+
+export const getUserSchema = z.object({
+  id: z.number()
+})
