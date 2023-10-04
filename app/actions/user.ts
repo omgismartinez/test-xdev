825aa1d50489058ccb216138f8b6a1298ed577af
omgismartinez <martnzomg@gmail.com>
2023-10-04 15:55:03 -0600

    🚀 feat (user): new user action

--- app/actions/user.ts
+++ app/actions/user.ts
@@ -1,13 +1,32 @@
 'use server'
 
 import { type z } from 'zod'
-import { type getUserSchema } from '@/lib/validations/user'
+import type {
+  userSchema,
+  getUserSchema
+} from '@/lib/validations/user'
 import { revalidatePath } from 'next/cache'
 
-export async function deleteUserAction (
-  input: z.infer<typeof getUserSchema>
+export async function newUserAction (
+  input: z.infer<typeof userSchema>
 ) {
-  console.log('deleteUserAction', input)
+  console.log('newUserAction', input)
+  const user = await fetch('https://fakestoreapi.com/users', {
+    method: 'POST',
+    body: JSON.stringify(input)
+  })
+
+  if (!user) {
+    throw new Error('User not found')
+  }
+
+  revalidatePath('/users')
+}
+
+export async function deleteUserAction (
+  input: z.infer<typeof getUserSchema
+  >
+) {
   const user = await fetch(`https://fakestoreapi.com/users/${input.id}`, {
     method: 'DELETE'
   })
