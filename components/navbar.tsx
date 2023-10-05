1e39fc88464d7feac20d0d6c4deab5b701a357bc
omgismartinez <martnzomg@gmail.com>
2023-10-04 23:31:07 -0600

    🚀 feat: navbar

---
+++ components/navbar.tsx
@@ -0,0 +1,45 @@
+'use client'
+
+import { userLogoutAction } from '@/app/actions/auth'
+import { LogOut } from 'lucide-react'
+import { useTransition } from 'react'
+import { Button } from './ui/button'
+
+export default function Navbar () {
+  const [isPending, startTransition] = useTransition()
+
+  const handleLogout = () => {
+    startTransition(async () => {
+      try {
+        await userLogoutAction()
+          .then(() => localStorage.removeItem('token'))
+          .then(() => window.location.reload())
+      } catch (error) {
+        console.error(error)
+      }
+    })
+  }
+  return (
+    <div className='flex justify-end pb-6'>
+      <Button
+        disabled={isPending}
+        onClick={handleLogout}
+        size='sm'
+      >
+        {isPending
+          ? <svg
+            viewBox='0 0 24 24'
+            fill='none'
+            stroke='currentColor'
+            strokeWidth='2'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            className='animate-spin inline-block w-4 h-4'
+          >
+            <path d='M21 12a9 9 0 1 1-6.219-8.56' />
+          </svg>
+          : <LogOut className='w-4 h-4' />}
+      </Button>
+    </div>
+  )
+}
