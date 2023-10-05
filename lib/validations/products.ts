9da2cb3841c0e25dfba16f4e8e7cb1e4d69adfa1
omgismartinez <martnzomg@gmail.com>
2023-10-04 18:37:51 -0600

    🌌 refactor (products): price coerce

--- lib/validations/products.ts
+++ lib/validations/products.ts
@@ -3,7 +3,7 @@ import { z } from 'zod'
 export const productSchema = z.object({
   id: z.number(),
   title: z.string(),
-  price: z.string(),
+  price: z.coerce.number(),
   category: z.string(),
   description: z.string(),
   image: z.string()
