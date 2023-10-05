4754256be0ac4e5d0d99bf51291e7c027961f24a
omgismartinez <martnzomg@gmail.com>
2023-10-04 23:27:41 -0600

    🌌 refactor (auth): restrictions removed

--- lib/validations/auth.ts
+++ lib/validations/auth.ts
@@ -1,16 +1,12 @@
 import { z } from 'zod'
 
-export const loginSchema = z.object({
+export const authSchema = z.object({
   username: z
     .string({
       required_error: 'Username is required'
-    })
-    .min(3)
-    .max(20),
+    }),
   password: z
     .string({
       required_error: 'Password is required'
     })
-    .min(8)
-    .max(100)
 })
