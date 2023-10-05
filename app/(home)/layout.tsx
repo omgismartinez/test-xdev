5d6cd65277c36d42d75c58773974967b0b2a0168
omgismartinez <martnzomg@gmail.com>
2023-10-04 23:39:12 -0600

    🌌 refactor (layout): navbar added

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,8 +1,10 @@
+import Navbar from '@/components/navbar'
 import React from 'react'
 
 export default function HomeLayout ({ children }: { children: React.ReactNode }) {
   return (
-    <main className='max-w-6xl mx-auto py-5 px-2'>
+    <main className='max-w-6xl mx-auto py-5 px-4'>
+      <Navbar />
       {children}
     </main>
   )
